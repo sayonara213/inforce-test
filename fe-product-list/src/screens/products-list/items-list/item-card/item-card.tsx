eabaef0a07e66550e739883b306c7d3a06362bb8
sayonara213 <maxsai567@gmail.com>
2024-02-29 19:50:38 +0200

    FE: handle delete product

--- fe-product-list/src/screens/products-list/items-list/item-card/item-card.tsx
+++ fe-product-list/src/screens/products-list/items-list/item-card/item-card.tsx
@@ -1,28 +1,66 @@
 import React from 'react';
 import { IProduct } from '../../../../types/product.types';
-import { Badge, Card, Group, Image, Text } from '@mantine/core';
+import { ActionIcon, Badge, Card, Group, Image, Text } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import styles from './item-card.module.scss';
+import { IconTrash } from '@tabler/icons-react';
+import { useDisclosure } from '@mantine/hooks';
+import { ConfirmationModal } from '../../../../components/confirmation-modal/confirmation-modal';
+import { useAppDispatch } from '../../../../hooks/reduxHooks';
+import { removeProduct } from '../../../../redux/products.slice';
 
 interface ItemCardProps {
   product: IProduct;
+  deleteProduct: (id: string) => void;
 }
 
-export const ItemCard: React.FC<ItemCardProps> = ({ product }) => {
+export const ItemCard: React.FC<ItemCardProps> = ({ product, deleteProduct }) => {
+  const [opened, { open, close }] = useDisclosure();
+  const dispatch = useAppDispatch();
+
+  const handleDelete = () => {
+    try {
+      deleteProduct(product.id);
+      dispatch(removeProduct(product.id));
+      close();
+    } catch {
+      console.log('Error');
+    }
+  };
+
   return (
-    <Card shadow='sm' padding={'lg'} radius={'sm'} withBorder>
-      <Card.Section style={{ overflow: 'hidden' }}>
-        <Link to={`/product/${product.id}`}>
-          <Image src={product.imageUrl} height={300} alt='Product image' className={styles.image} />
-        </Link>
-      </Card.Section>
-      <Group justify='space-between' mt='md' mb='xs'>
-        <Text fw={500}>{product.name}</Text>
-        <Badge color='pink'>{product.count}</Badge>
-      </Group>
-      <Text size='sm' c='dimmed'>
-        weight: {product.weight}g
-      </Text>
-    </Card>
+    <>
+      <ConfirmationModal
+        opened={opened}
+        onClose={close}
+        onConfirm={handleDelete}
+        title='Delete Product'
+        description='Are you sure you want to delete this product'
+      />
+      <Card shadow='sm' padding={'lg'} radius={'sm'} withBorder>
+        <Card.Section style={{ overflow: 'hidden', position: 'relative' }}>
+          <Link to={`/product/${product.id}`}>
+            <Image
+              src={product.imageUrl}
+              height={300}
+              alt='Product image'
+              className={styles.image}
+            />
+          </Link>
+          <div className={styles.actions}>
+            <ActionIcon onClick={open}>
+              <IconTrash size={20} />
+            </ActionIcon>
+          </div>
+        </Card.Section>
+        <Group justify='space-between' mt='md' mb='xs'>
+          <Text fw={500}>{product.name}</Text>
+          <Badge color='pink'>{product.count}</Badge>
+        </Group>
+        <Text size='sm' c='dimmed'>
+          weight: {product.weight}g
+        </Text>
+      </Card>
+    </>
   );
 };
--- fe-product-list/src/screens/products-list/items-list/items-list.tsx
+++ fe-product-list/src/screens/products-list/items-list/items-list.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { IProduct } from '../../../types/product.types';
 import { Card, SimpleGrid } from '@mantine/core';
 import { ItemCard } from './item-card/item-card';
+import { useDeleteProductMutation } from '../../../redux/services/products.api';
 
 interface ItemsListProps {
   products: IProduct[];
 }
 
 export const ItemsList: React.FC<ItemsListProps> = ({ products }) => {
+  const [serverDeleteProduct] = useDeleteProductMutation();
+
   return (
     <SimpleGrid cols={3}>
       {products.map((product) => (
-        <ItemCard key={product.id} product={product} />
+        <ItemCard key={product.id} product={product} deleteProduct={serverDeleteProduct} />
       ))}
     </SimpleGrid>
   );
